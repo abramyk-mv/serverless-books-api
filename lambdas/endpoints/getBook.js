ce19cb7ffd5dc0ceac33c848513004dd68fff42d
Mariia Abramyk <abramyk.mv@gmail.com>
2020-05-07 04:22:22 +0300

    Add documentation (#2)
    * Add documentation.
    
    * Markup format.

--- lambdas/endpoints/getBook.js
+++ lambdas/endpoints/getBook.js
@@ -26,25 +26,3 @@ exports.handler = async event => {
 
     return Responses._200(book);
 };
-
-// todo: move it to seed data
-const data = [
-    {
-        uuid: '44343595-2c8e-40e7-a063-40b28970eec9',
-        name: 'Amazon Web Services in Action',
-        releaseDate: 1445118568000, // note:  October 17, 2015 9:49:28 PM (GMT)
-        author: 'Andreas Witting and Michael Wittig',
-    },
-    {
-        uuid: 'f3d7757b-5549-4c73-9bf7-64c852376a18',
-        name: 'Mastering AWS Development',
-        releaseDate: 1434750568000, // note:  June 19, 2015 9:49:28 PM (GMT)
-        author: 'Uchit Vyas',
-    },
-    {
-        uuid: '7216412d-c07b-45f1-8b0c-5252ac3c6826',
-        name: 'Implementing Cloud Design Patterns for AWS',
-        releaseDate: 1556660968000, // note:  April 30, 2019 9:49:28 PM (GMT)
-        author: 'Marcus Young',
-    },
-];
